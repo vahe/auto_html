1f1cc1bb6dc6a495cd36f196ce613fbcb40ed911
Vahe Khachikyan <vahe@live.ca>
2013-05-15 00:29:00 -0400

    tag.rb initial commit

---
+++ lib/auto_html/filters/tag.rb
@@ -0,0 +1,8 @@
+AutoHtml.add_filter(:tag).with({}) do |text, options|
+  regex = /(\A|\z|\s)(#|＃)([a-z0-9_]*[a-z_][a-z0-9_]*)/i
+  text.gsub(regex) do
+    tag = $3
+    %{ <span class="secondary label #{tag}">#{tag}</span> }
+  end
+end
+
