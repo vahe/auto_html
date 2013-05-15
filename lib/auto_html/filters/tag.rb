8fceb61532d87936944c99b6ad0486c741f61123
Vahe Khachikyan <vahe@live.ca>
2013-05-15 00:39:49 -0400

    fixed #

--- lib/auto_html/filters/tag.rb
+++ lib/auto_html/filters/tag.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:tag).with({}) do |text, options|
-  regex = /(\A|\z|\s)(#|＃)([a-z0-9_]*[a-z_][a-z0-9_]*)/i
+  regex = /(\A|\z|\s)(#)([a-z0-9_]*[a-z_][a-z0-9_]*)/i
   text.gsub(regex) do
     tag = $3
     %{ <span class="secondary label #{tag}">#{tag}</span> }
